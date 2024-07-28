a363adadfa6a9b02716a519322889a470546c77a
KAMAH <limitlesskds@gmail.com>
2024-07-29 00:17:52 +0100

    Aded exception filter to prevent server crash

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { FilterExceptions } from './utils/exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  
+  // FILTER EXCEPTINS AND PREVENT SERVER CRASHES
+  app.useGlobalFilters(new FilterExceptions())
   await app.listen(process.env.PORT);
 }
 bootstrap();
